c2f6898441716bc40d7a7a67932e81b644b5afe2
miller09 <120627736+MillerAndrey@users.noreply.github.com>
2023-04-12 18:44:46 +0300

    Add files via upload

---
+++ Spring_2_3_1/src/main/java/web/config/AppConfig.java
@@ -0,0 +1,66 @@
+package web.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.core.env.Environment;
+import org.springframework.jdbc.datasource.DriverManagerDataSource;
+import org.springframework.orm.jpa.JpaTransactionManager;
+import org.springframework.orm.jpa.LocalContainerEntityManagerFactoryBean;
+import org.springframework.orm.jpa.vendor.HibernateJpaVendorAdapter;
+import org.springframework.transaction.PlatformTransactionManager;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
+
+import javax.sql.DataSource;
+import java.util.Properties;
+
+
+@Configuration
+@PropertySource("classpath:db.properties")
+@EnableTransactionManagement
+@ComponentScan(value = "web")
+public class AppConfig {
+    private Environment env;
+
+    public AppConfig(Environment env) {
+        this.env = env;
+    }
+
+    @Bean
+    public LocalContainerEntityManagerFactoryBean getEntityManagerFactoryBean() {
+        LocalContainerEntityManagerFactoryBean lcemfb = new LocalContainerEntityManagerFactoryBean();
+        lcemfb.setDataSource(dataSource());
+        lcemfb.setJpaVendorAdapter(new HibernateJpaVendorAdapter());
+        lcemfb.setPackagesToScan("web");
+        lcemfb.setJpaProperties(hibernateProperties());
+        return lcemfb;
+    }
+    @Bean
+    public DataSource dataSource() {
+        DriverManagerDataSource dataSource = new DriverManagerDataSource();
+        dataSource.setDriverClassName(env.getProperty("db.driver"));
+        dataSource.setUrl(env.getProperty("db.url"));
+        dataSource.setUsername(env.getProperty("db.username"));
+        dataSource.setPassword(env.getProperty("db.password"));
+        return dataSource;
+    }
+
+    @Bean
+    public PlatformTransactionManager getTransactionManager() {
+        JpaTransactionManager transactionManager = new JpaTransactionManager();
+        transactionManager.
+                setEntityManagerFactory(getEntityManagerFactoryBean().getObject());
+        return transactionManager;
+    }
+
+    private Properties hibernateProperties() {
+        Properties properties = new Properties();
+        //properties.put("hibernate.dialect", env.getRequiredProperty("hibernate.dialect"));
+        properties.put("hibernate.show_sql", env.getRequiredProperty("hibernate.show_sql"));
+        properties.put("hibernate.hbm2ddl.auto", env.getRequiredProperty("hibernate.hbm2ddl.auto"));
+        return properties;
+    }
+
+
+}
---
+++ Spring_2_3_1/src/main/java/web/dao/UserDaoImpl.java
@@ -0,0 +1,38 @@
+package web.dao;
+
+import org.springframework.stereotype.Repository;
+import web.model.User;
+
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+import javax.persistence.TypedQuery;
+import java.util.List;
+
+@Repository
+public class UserDaoImpl implements UserDao {
+    @PersistenceContext
+    private EntityManager entityManager;
+
+
+    @Override
+    public void saveUser(User user) {
+        entityManager.merge(user);
+    }
+
+    @Override
+    public void removeUserById(long id) {
+        entityManager.remove(entityManager.find(User.class, id));
+    }
+
+    @Override
+    public List<User> getAllUsers() {
+        TypedQuery<User> query = entityManager.createQuery("select u from User u", User.class);
+        return query.getResultList();
+    }
+
+
+    @Override
+    public User getUser(Long id) {
+        return entityManager.find(User.class, id);
+    }
+}
